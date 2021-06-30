6270ddf43f0810cbc268a1d2b3ef4177bff006fb
Jetal C. Mali <malijetal1234@gmail.com>
2021-06-30 23:59:34 +0530

    Added sever app

---
+++ random-word-server/README.md
@@ -0,0 +1,21 @@
+# random-word-server
+
+### This is a server side application which fetch random five letter words for Joto App.
+
+## Installing
+
+Clone or fork the course repository found at [https://github.com/MaliJetal/react-testing-projects.git]
+cd into the random-word-server directory (where this README is located).
+Run npm install
+
+## Starting the server
+
+Run npm start. The server will be found at [http://localhost:3030]
+
+## Using the server
+
+This server has one purpose: return a response containing a random five-letter word (with no duplicate letters) at the / route.
+
+## Testing
+
+To test, run npm test.
---
+++ random-word-server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+//CORS for react app, assuming at port 3000
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    credentials: true,
+  })
+);
+
+//read words from json file
+const filecontents = fs.readFileSync("./five-letter-words.json", "utf-8");
+const words = JSON.parse(filecontents);
+const { fiveLetterWords } = words;
+
+app.get("/", (req, res) => {
+  //select a random word
+  const selectWord =
+    fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+
+  //return it as the response
+  res.send(selectWord);
+});
+
+app.listen(3030, () => console.log("word server listening at port 3030"));
+
+module.exports = app;
